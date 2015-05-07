cd16befc7bccee76065787e13c5d6bf679bfe789
Rufus Mbugua <mbuguarufus@gmail.com>
2015-05-07 10:45:52 +0300

    Moved GlyphIcons
    Signed-off-by: Rufus Mbugua <mbuguarufus@gmail.com>

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -1,20 +1,20 @@
 app.controller(
-  "usersCtrl", ['$scope', '$filter', '$timeout', '$state', 'Restangular',
-    'md5',
+	"usersCtrl", ['$scope', '$filter', '$timeout', '$state', 'Restangular',
+		'md5',
 
-    function(scope, filter, timeout, state, Restangular, md5) {
-      var users = Restangular.all('users');
+		function(scope, filter, timeout, state, Restangular, md5) {
+			var users = Restangular.all('users');
 
 
-      scope.login = function login() {
-        scope.user.password = md5.createHash(scope.user.password || '');
-        users.post(scope.user).then(function(response) {
-          if (response[0]) {
-            state.
-          }
-        });
-      }
+			scope.login = function login() {
+				scope.user.password = md5.createHash(scope.user.password || '');
+				users.post(scope.user).then(function(response) {
+					// if (response[0]) {
+					//   state.
+					// }
+				});
+			}
 
-    }
-  ]
+		}
+	]
 );
