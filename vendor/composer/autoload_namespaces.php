76e5a51e07c7f3778aa2a2862969d1f7f203a237
Rufus Mbugua <mbuguarufus@gmail.com>
2015-05-19 16:12:07 +0300

    Ignores

--- vendor/composer/autoload_namespaces.php
+++ vendor/composer/autoload_namespaces.php
@@ -7,4 +7,5 @@ $baseDir = dirname($vendorDir);
 
 return array(
     'org\\bovigo\\vfs' => array($vendorDir . '/mikey179/vfsStream/src/main/php'),
+    'Gaufrette' => array($vendorDir . '/knplabs/gaufrette/src'),
 );
