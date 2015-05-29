7e18155f8e6920bff6120a1daff986167e8f38e5
Rufus Mbugua <mbuguarufus@gmail.com>
2015-05-29 08:42:58 +0300

    Tying in changes to home page.

--- dist/app.js
+++ dist/app.js
@@ -437,77 +437,77 @@ app.value('froalaConfig', {
 	]
 );
 ;app.controller(
-  "homeCtrl", ['$scope', '$filter', '$timeout', '$state', 'Restangular',
-    function(scope, filter, timeout, state, Restangular) {
-      var front = Restangular.all('pages?format=json');
+	"homeCtrl", ['$scope', '$filter', '$timeout', '$state', 'Restangular',
+		function(scope, filter, timeout, state, Restangular) {
+			var front = Restangular.all('pages?format=json');
 
-      var Slides = Restangular.all('files/slides?format=json');
+			var Slides = Restangular.all('files/slides?format=json');
 
-      var Queries = Restangular.all('queries?format=json');
-      loadImages();
-      loadContent();
-      scope.content = [];
-      scope.query = [];
-      scope.alerts = [];
-      /**
-       * [loadImages description]
-       */
-      function loadImages() {
-        Slides.customGET().then(function(slides) {
-          scope.slides = slides.data;
-        });
-      }
+			var Queries = Restangular.all('queries?format=json');
+			loadImages();
+			loadContent();
+			scope.content = [];
+			scope.query = [];
+			scope.alerts = [];
+			/**
+			 * [loadImages description]
+			 */
+			function loadImages() {
+				Slides.customGET().then(function(slides) {
+					scope.slides = slides.data;
+				});
+			}
 
-      /**
-       * [loadContent description]
-       */
-      function loadContent() {
+			/**
+			 * [loadContent description]
+			 */
+			function loadContent() {
 
-        front.getList().then(function(content) {
-          angular.forEach(content, function(value, key) {
-            switch (value.name) {
-              case "Welcome to NQCL":
-                scope.content.welcome = value;
-                break;
-              case "Our Services":
-                scope.content.services = value;
-                break;
-              case "Contact Us":
-                scope.content.contact = value;
-                break;
-              default:
+				front.getList().then(function(content) {
+					angular.forEach(content, function(value, key) {
+						switch (value.name) {
+							case "Welcome to NQCL":
+								scope.content.welcome = value;
+								break;
+							case "Our Services":
+								scope.content.services = value;
+								break;
+							case "Contact Us":
+								scope.content.contact = value;
+								break;
+							default:
 
-                break;
+								break;
 
-            }
+						}
 
-          });
-        });
-      }
+					});
+				});
+			}
 
-      /**
-       * [sendQuery description]
-       */
-      scope.sendQuery = function sendQuery() {
-        // console.log(scope.query);
-        Queries.post(scope.query).then(function(response) {
-          var alert = {
-            type: 'success',
-            msg: response
-          }
-          scope.alerts.push(alert);
-        });;
-      }
+			/**
+			 * [sendQuery description]
+			 */
+			scope.sendQuery = function sendQuery() {
+				// console.log(scope.query);
+				Queries.post(scope.query).then(function(response) {
+					var alert = {
+							type: 'success',
+							msg: response
+						}
+						// scope.alerts.push(alert);
+				});;
+			}
 
-      /**
-       * [closeAlert description]
-       * @param {[type]} index [description]
-       */
-      scope.closeAlert = function(index) {
-        scope.alerts.splice(index, 1);
-      };
-    }
-  ]
+			/**
+			 * [closeAlert description]
+			 * @param {[type]} index [description]
+			 */
+			scope.closeAlert = function(index) {
+				scope.alerts.splice(index, 1);
+			};
+		}
+	]
 );
 ;app.controller(
 	"usersCtrl", ['$scope', '$filter', '$timeout', '$state', 'Restangular',
@@ -1368,6 +1368,10 @@ angular.module("../app/partials/home/index.html", []).run(["$templateCache", fun
     "  <section class=\"content small\">\n" +
     "    <h1>{{content.contact.name}}</h1>\n" +
     "    <div class=\"description\" ng-bind-html=\"content.contact.content[0].body\"></div>\n" +
+    "\n" +
+    "    <div class='btn-group'>\n" +
+    "      <a href=\"\" class=\"btn btn-sm btn-view\" ui-sref=\"public.contact\"><i class='ion-help-circled'></i>Send Query</a>\n" +
+    "    </div>\n" +
     "  </section>\n" +
     "</div>\n" +
     "");
